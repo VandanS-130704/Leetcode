231de695628a4ec1143d00adb25c806e57ac5ae3
VandanS-130704 <vandanvs13@gmail.com>
2025-03-30 17:08:13 +0530

    Time: 283 ms (99.14%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+WITH base AS(SELECT requester_id id 
+             FROM RequestAccepted
+             UNION ALL
+             SELECT accepter_id id 
+             FROM RequestAccepted)
+
+
+SELECT id, count(*) num  
+FROM base 
+GROUP BY 1 
+ORDER BY 2 DESC
+LIMIT 1;
