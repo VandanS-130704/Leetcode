1bc748104a94f7c64c1fc95efca2657ddf2bafdc
VandanS-130704 <vandanvs13@gmail.com>
2025-04-30 19:56:26 +0530

    Time: 1091 ms (50.86%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-# Write your MySQL query statement below
 SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.student_id) AS attended_exams
 FROM Students s
 CROSS JOIN Subjects sub
