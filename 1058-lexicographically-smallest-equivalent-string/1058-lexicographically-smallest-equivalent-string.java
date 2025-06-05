f03a52965506f139a8f36bb089ae659a660bc263
VandanS-130704 <vandanvs13@gmail.com>
2025-06-06 04:13:37 +0530

    Time: 2 ms (96.88%), Space: 42 MB (63.07%) - LeetHub

---
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.java
@@ -0,0 +1,52 @@
+class Solution 
+{
+    int[] parent = new int[26];
+    public String smallestEquivalentString(String s1, String s2, String baseStr) 
+    {
+        for (int i = 0; i < 26; i++) 
+        {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < s1.length(); i++) 
+        {
+            union(s1.charAt(i) - 'a', s2.charAt(i) - 'a');
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (char ch : baseStr.toCharArray()) 
+        {
+            result.append((char)(find(ch - 'a') + 'a'));
+        }
+
+        return result.toString();
+    }
+
+    private int find(int x) 
+    {
+        if (parent[x] != x) 
+        {
+            parent[x] = find(parent[x]);
+        }
+        
+        return parent[x];
+    }
+
+    private void union(int x, int y) 
+    {
+        int px = find(x), py = find(y);
+        if (px == py)
+        {
+            return;
+        }
+
+        if (px < py) 
+        {
+            parent[py] = px;
+        } 
+        else 
+        {
+            parent[px] = py;
+        }
+    }
+}
